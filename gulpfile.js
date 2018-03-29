ca19d483be325318369dd9ee6a2677d336acea18
Phil Hawksworth <phil@netify.com>
2018-03-29 15:44:58 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp        = require("gulp");
+// var shell       = require('gulp-shell');
+// var clean       = require('gulp-clean');
+// var runSequence = require('run-sequence');
+// var fs          = require('fs');
+
+
+// what goes where?
+var buildSrc = "src";
+var buildDest = "dist";
+var localCache = "cache";
+var prodCache = " /opt/build/cache/";
+var cache;
+
+// automatically set the correct path for our cache manipulations
+if(process.env.URL) {
+  cache = prodCache;
+} else {
+  cache = localCache;
+}
+
+
+// Compile SCSS files to CSS
+gulp.task("generate:docs", function () {
+  gulp.src(buildSrc + "/docs/**/*")
+    .pipe(gulp.dest(buildDest + "/docs"))
+    .pipe(gulp.dest(cache + "/docs"))
+});
+
+gulp.task("get:docs", function () {
+  gulp.src(cache + "/docs/**/*")
+    .pipe(gulp.dest(buildDest + "/docs"))
+});
+
