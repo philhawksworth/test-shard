0457f05cd0dda2b7f15b18556dd32748a803a5b9
Phil Hawksworth <phil@netify.com>
2018-03-29 16:16:52 -0500

    logging the cache content

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp        = require("gulp");
-// var shell       = require('gulp-shell');
+var shell       = require('gulp-shell');
 // var clean       = require('gulp-clean');
 // var runSequence = require('run-sequence');
 // var fs          = require('fs');
@@ -20,21 +20,26 @@ if(process.env.URL) {
 }
 
 
-// Compile SCSS files to CSS
+// put the dcs files into the cache and dist folder
 gulp.task("generate:docs", function () {
 
-  console.log(`STASH DOCS ASSETS AT ${cache}`);
+  console.log(`STASH DOCS ASSETS AT ${cache}/docs`);
 
   gulp.src(buildSrc + "/docs/**/*")
     .pipe(gulp.dest(buildDest + "/docs"))
     .pipe(gulp.dest(cache + "/docs"))
 });
 
+
+// get the dios files from the cache folder
 gulp.task("get:docs", function () {
 
-  console.log(`GET DOCS ASSETS FROM ${cache}`);
+  console.log(`GET DOCS ASSETS FROM ${cache}/docs`);
 
   gulp.src(cache + "/docs/**/*")
-    .pipe(gulp.dest(buildDest + "/docs"))
+    .pipe(gulp.dest(buildDest + "/docs"));
 });
 
+
+
+gulp.task('list', shell.task(`tree ${cache}`));
